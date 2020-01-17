4369785b73c9a1ff7b8fc9a6f49cc881d9195b93
NasgulNexus <moctegor@gmail.com>
2020-01-17 20:28:29 +0300

    домашняя работа по питону

--- core/calculator.py
+++ core/calculator.py
@@ -4,12 +4,32 @@ def calculator(a, b, operation):
     # Здесь нужно реализовать функцию,
     # которая реализует основные арифметические операции между числами: +, -, /, *, **.
     # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
-
+    return {
+        '+':lambda a,b: a+b,
+        '-':lambda a,b: a - b,
+        '/':lambda a,b: a / b,
+        '*':lambda a,b: a * b,
+        '**':lambda a,b: a ** b
+    }[operation](a,b)
 
 if __name__ == '__main__':
-    a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
-    b = int(input('Введите число: '))
-    operation = input('Введите операцию')
+    while True:
+            try:
+            a = int(input('Введите число: '))
+            break
+        except ValueError:
+            print("Ошибка! Неверный ввод")
+    while True:
+        try:
+            b = int(input('Введите число: '))
+            break
+        except ValueError:
+            print("Ошибка! Неверный ввод")
+    while True:
+        try:
+           operation = input('Введите операцию: ')
+            break
+        except ValueError:
+            print("Ошибка! Неверный ввод") 
 
     print('Результат: ', calculator(a, b, operation))
--- core/generators.py
+++ core/generators.py
@@ -1,5 +1,10 @@
+from random import randint
 # написать генераторную функцию, которая принимает число N возвращает N рандомных чисел от 1 до 100
 def gen(N):
-    pass
-
+    answer = []
+    for i in range(N):
+        answer.append(randint(1, 100))
+    return answer
 # написать генераторное выражение, которое делает то же самое
+generatorAnswer = (randint(1, 100) for i in range(N))
+N=input
